5fe2f558c0fb51ee707b27e2ce4545218f0de4f2
adeel-ahmad-awan <destontrace@gmail.com>
2018-05-10 10:52:53 +0500

    small adjustments in driver.php

--- 3_datastructure/driver.php
+++ 3_datastructure/driver.php
@@ -8,17 +8,74 @@
   $linkedlist->addNodeToTail(12);
   $linkedlist->addNodeToTail(11);
   $linkedlist->addNodeToTail(10);
+  $linkedlist->addNodeToTail(8);
+  $linkedlist->addNodeToTail(45);
+  $linkedlist->addNodeToTail(80);
+  $linkedlist->addNodeToTail(67);
+  $linkedlist->addNodeToTail(33);
+  $linkedlist->addNodeToTail(43);
+  $linkedlist->addNodeToTail(43);
 
-  $linkedlist->printAllLinkedList();
+
   echo 'Node count = ' . $linkedlist->getLinkedListNodesCount() . PHP_EOL;
+  $linkedlist->printAllLinkedList();
 
+  echo 'removing 10 node by value' . PHP_EOL;
   if ($linkedlist->removeNodeByValue(10)) {
     echo 'sucessfully removed' . PHP_EOL;
   }
   else {
     echo 'removal unsucessfull' . PHP_EOL;
   }
-  $linkedlist->printAllLinkedList();
+
   echo 'Node count = ' . $linkedlist->getLinkedListNodesCount() . PHP_EOL;
+  $linkedlist->printAllLinkedList();
+
+
+  echo 'removing 43 node by value' . PHP_EOL;
+  if ($linkedlist->removeNodeByValue(43)) {
+    echo 'sucessfully removed' . PHP_EOL;
+  }
+  else {
+    echo 'removal unsucessfull' . PHP_EOL;
+  }
+
+  echo 'Node count = ' . $linkedlist->getLinkedListNodesCount() . PHP_EOL;
+  $linkedlist->printAllLinkedList();
+
+  echo 'removing 99 node by value' . PHP_EOL;
+  if ($linkedlist->removeNodeByValue(10)) {
+    echo 'sucessfully removed' . PHP_EOL;
+  }
+  else {
+    echo 'removal unsucessfull' . PHP_EOL;
+  }
+
+  echo 'Node count = ' . $linkedlist->getLinkedListNodesCount() . PHP_EOL;
+  $linkedlist->printAllLinkedList();
+
+  echo 'removing 0 node by position' . PHP_EOL;
+  if ($linkedlist->removeNodeByPoistion(0)) {
+    echo 'sucessfully removed' . PHP_EOL;
+  }
+  else {
+    echo 'removal unsucessfull' . PHP_EOL;
+  }
+
+  echo 'Node count = ' . $linkedlist->getLinkedListNodesCount() . PHP_EOL;
+  $linkedlist->printAllLinkedList();
+
+
+  echo 'removing 6 node by position' . PHP_EOL;
+  if ($linkedlist->removeNodeByPoistion(6)) {
+    echo 'sucessfully removed' . PHP_EOL;
+  }
+  else {
+    echo 'removal unsucessfull' . PHP_EOL;
+  }
+
+  echo 'Node count = ' . $linkedlist->getLinkedListNodesCount() . PHP_EOL;
+  $linkedlist->printAllLinkedList();
+
 
 ?>
